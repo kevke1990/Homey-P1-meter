87b107c79027266e174e520f2e78ab7a212088b1
kevke1990 <otten.kevin@gmail.com>
2026-08-24 14:58:01 +0200

    Update driver.js

--- com.generic.p1meter/drivers/p1_dongle/driver.js
+++ com.generic.p1meter/drivers/p1_dongle/driver.js
@@ -4,28 +4,38 @@ const Homey = require('homey');
 
 class P1DongleDriver extends Homey.Driver {
   async onInit() {
-    this.log('P1 Dongle Driver has been initialized');
+    this.log('P1 Dongle Driver is opgestart');
+
+    // Controleer direct bij het starten van de driver of het apparaat al bestaat
+    this.ensureDeviceExists();
   }
 
-  async onPair(session) {
-    // We maken een handler aan genaamd 'save_config' die luistert naar de Save knop
-    session.setHandler('save_config', async (data) => {
-        if (!data.ip) {
-            throw new Error('IP-adres is verplicht.');
-        }
+  async ensureDeviceExists() {
+    try {
+      const devices = this.getDevices();
 
-        // We sturen het geformatteerde apparaat terug naar het koppelingsscherm
-        return {
-            name: 'P1 Meter (' + data.ip + ')',
-            data: { 
-                id: data.ip // Unieke ID van het apparaat
-            },
-            settings: { 
-                ip: data.ip, 
-                polling_interval: Number(data.interval) || 10 
-            }
-        };
-    });
+      if (devices.length === 0) {
+        this.log('Geen P1 meter gevonden in deze driver, automatisch aanmaken...');
+
+        // Dit is de officiële SDK v3 manier om vanuit de driver een apparaat toe te voegen
+        await this.createDevice({
+          name: 'P1 Meter (Chargee Sparky)',
+          data: {
+            id: 'sparky_p1_192.168.8.224'
+          },
+          settings: {
+            ip: '192.168.8.224',
+            polling_interval: 10
+          }
+        });
+
+        this.log('P1 Meter is succesvol aangemaakt door de driver!');
+      } else {
+        this.log('P1 Meter bestaat al.');
+      }
+    } catch (err) {
+      this.error('Fout bij automatisch aanmaken vanuit driver:', err);
+    }
   }
 }
 
