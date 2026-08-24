4f68f229589adbf09b8e626524b9f2fa281ec88c
kevke1990 <otten.kevin@gmail.com>
2026-08-24 15:25:34 +0200

    Update driver.js

--- com.generic.p1meter/drivers/p1_dongle/driver.js
+++ com.generic.p1meter/drivers/p1_dongle/driver.js
@@ -8,27 +8,17 @@ class P1DongleDriver extends Homey.Driver {
   }
 
   async onPair(session) {
-    let pairingIp = '192.168.8.224';
-    let pairingPort = 3602;
-
-    // 1. Vang de gegevens op die je in de wizard invult
-    session.setHandler('set_settings', async (data) => {
-      if (data.ip) pairingIp = data.ip;
-      if (data.port) pairingPort = parseInt(data.port, 10);
-      return true;
-    });
-
-    // 2. Voeg het apparaat toe zodra je op Volgende/Toevoegen drukt
     session.setHandler('list_devices', async () => {
+      this.log('Apparaatlijst opgevraagd voor Chargee Sparky...');
       return [
         {
-          name: `Chargee Sparky P1 (${pairingIp})`,
+          name: 'Chargee Sparky P1 Meter',
           data: {
-            id: `sparky_p1_${pairingIp}`
+            id: 'sparky_p1_192.168.8.224'
           },
           settings: {
-            ip: pairingIp,
-            port: pairingPort
+            ip: '192.168.8.224',
+            port: 3602
           }
         }
       ];
@@ -36,4 +26,4 @@ class P1DongleDriver extends Homey.Driver {
   }
 }
 
-module.exports = P1DongleDevice => P1DongleDriver; // of standaard module.exports = P1DongleDriver;
+module.exports = P1DongleDriver;
