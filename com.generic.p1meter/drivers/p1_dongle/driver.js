99c5b0a83bc128cd698944aac407c73273498bee
kevke1990 <otten.kevin@gmail.com>
2026-08-24 14:23:08 +0200

    Update driver.js

--- com.generic.p1meter/drivers/p1_dongle/driver.js
+++ com.generic.p1meter/drivers/p1_dongle/driver.js
@@ -3,20 +3,29 @@
 const Homey = require('homey');
 
 class P1DongleDriver extends Homey.Driver {
-  /**
-   * onInit is called when the driver is initialized
-   */
   async onInit() {
     this.log('P1 Dongle Driver has been initialized');
   }
 
-  /**
-   * Handle pairing process
-   * We need to define manual IP pairing
-   */
   async onPair(session) {
-    // We don't need a save_settings handler here because we are directly
-    // creating the device via Homey.createDevice() from the frontend pairing view (settings.html).
+    // We maken een handler aan genaamd 'save_config' die luistert naar de Save knop
+    session.setHandler('save_config', async (data) => {
+        if (!data.ip) {
+            throw new Error('IP-adres is verplicht.');
+        }
+
+        // We sturen het geformatteerde apparaat terug naar het koppelingsscherm
+        return {
+            name: 'P1 Meter (' + data.ip + ')',
+            data: { 
+                id: data.ip // Unieke ID van het apparaat
+            },
+            settings: { 
+                ip: data.ip, 
+                polling_interval: Number(data.interval) || 10 
+            }
+        };
+    });
   }
 }
 
