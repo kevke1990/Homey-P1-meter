b0c6687c9fd9edcd35280585460cb96fae511ed7
kevke1990 <otten.kevin@gmail.com>
2026-08-24 15:13:01 +0200

    Update driver.js

--- com.generic.p1meter/drivers/p1_dongle/driver.js
+++ com.generic.p1meter/drivers/p1_dongle/driver.js
@@ -5,13 +5,19 @@ const Homey = require('homey');
 class P1DongleDriver extends Homey.Driver {
   async onInit() {
     this.log('P1 Dongle Driver is opgestart');
+
+    // Automatische bypass: direct aanmaken als het apparaat nog niet bestaat
+    this.checkAndCreateDevice();
   }
 
-  async onPair(session) {
-    session.setHandler('list_devices', async () => {
-      this.log('Koppel-lijst opgevraagd voor Chargee Sparky...');
-      return [
-        {
+  async checkAndCreateDevice() {
+    try {
+      const devices = this.getDevices();
+      if (devices.length === 0) {
+        this.log('Geen apparaten gevonden, apparaat direct registreren in Homey...');
+        
+        // De correcte SDK v3 achterdeur om een apparaat programmatisch toe te voegen
+        await this.addDevice({
           name: 'Chargee Sparky P1 Meter',
           data: {
             id: 'sparky_p1_192.168.8.224'
@@ -21,9 +27,15 @@ class P1DongleDriver extends Homey.Driver {
             port: 3602,
             polling_interval: 10
           }
-        }
-      ];
-    });
+        });
+        
+        this.log('Apparaat is succesvol automatisch toegevoegd!');
+      } else {
+        this.log('Apparaat bestaat al.');
+      }
+    } catch (err) {
+      this.error('Fout bij automatisch toevoegen:', err);
+    }
   }
 }
 
