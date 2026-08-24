57dae801b345b5c3098948afb7ac1aa33c4d2035
kevke1990 <otten.kevin@gmail.com>
2026-08-24 14:59:40 +0200

    Update driver.js

--- com.generic.p1meter/drivers/p1_dongle/driver.js
+++ com.generic.p1meter/drivers/p1_dongle/driver.js
@@ -5,37 +5,25 @@ const Homey = require('homey');
 class P1DongleDriver extends Homey.Driver {
   async onInit() {
     this.log('P1 Dongle Driver is opgestart');
-
-    // Controleer direct bij het starten van de driver of het apparaat al bestaat
-    this.ensureDeviceExists();
   }
 
-  async ensureDeviceExists() {
-    try {
-      const devices = this.getDevices();
-
-      if (devices.length === 0) {
-        this.log('Geen P1 meter gevonden in deze driver, automatisch aanmaken...');
-
-        // Dit is de officiële SDK v3 manier om vanuit de driver een apparaat toe te voegen
-        await this.createDevice({
-          name: 'P1 Meter (Chargee Sparky)',
-          data: {
-            id: 'sparky_p1_192.168.8.224'
-          },
-          settings: {
-            ip: '192.168.8.224',
-            polling_interval: 10
-          }
-        });
-
-        this.log('P1 Meter is succesvol aangemaakt door de driver!');
-      } else {
-        this.log('P1 Meter bestaat al.');
+  /**
+   * Deze ingebouwde SDK v3 methode toont automatisch een lijst 
+   * met apparaten in de Homey app die je direct kunt toevoegen.
+   */
+  async onPairListDevices() {
+    return [
+      {
+        name: 'P1 Meter (Chargee Sparky)',
+        data: {
+          id: 'sparky_p1_192.168.8.224'
+        },
+        settings: {
+          ip: '192.168.8.224',
+          polling_interval: 10
+        }
       }
-    } catch (err) {
-      this.error('Fout bij automatisch aanmaken vanuit driver:', err);
-    }
+    ];
   }
 }
 
