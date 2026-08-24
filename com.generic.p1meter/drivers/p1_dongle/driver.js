356143bcc2d128f9a012e7731b05a91af307041c
kevke1990 <otten.kevin@gmail.com>
2026-08-24 15:05:57 +0200

    Update driver.js

--- com.generic.p1meter/drivers/p1_dongle/driver.js
+++ com.generic.p1meter/drivers/p1_dongle/driver.js
@@ -7,20 +7,22 @@ class P1DongleDriver extends Homey.Driver {
     this.log('P1 Dongle Driver is opgestart');
   }
 
-  async onPairListDevices() {
-    return [
-      {
-        name: 'Chargee Sparky P1 Meter',
-        data: {
-          id: 'sparky_p1_192.168.8.224'
-        },
-        settings: {
-          ip: '192.168.8.224',
-          port: 3602,
-          polling_interval: 10
+  async onPair(session) {
+    session.setHandler('list_devices', async () => {
+      return [
+        {
+          name: 'Chargee Sparky P1 Meter',
+          data: {
+            id: 'sparky_p1_192.168.8.224'
+          },
+          settings: {
+            ip: '192.168.8.224',
+            port: 3602,
+            polling_interval: 10
+          }
         }
-      }
-    ];
+      ];
+    });
   }
 }
 
